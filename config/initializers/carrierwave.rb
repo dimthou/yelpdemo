7dd791e8576785a3434b35919fed9cf915439ec4
dim thou <dimthou0304.td@gmail.com>
2016-07-19 21:41:27 +0700

    CarrierWave Update provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
-  
+#  config.fog_provider = 'fog-google'                        # required
   config.fog_credentials = {
     provider:                         'Google',
     google_storage_access_key_id:     'GOOG7UFL3653ETM2JC62',
     google_storage_secret_access_key: 'p10AwOy8u2CZQWlYvr0La7FV/09vrGqoVj6I1doJ'
   }
-  config.fog_directory = 'yelpdemo-1377'
+  config.fog_directory = 'yelpdemo-1377.appspot.com'
 end
