dca0dcf8be840be3ec09c47d7bf33228c5cc4878
dim thou <dimthou0304.td@gmail.com>
2016-09-15 14:15:59 +0700

    link review with restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews, execpt: [:show, :index]
+  
   devise_for :users
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, execpt: [:show, :index]
+  end
   get 'pages/about'
 
   get 'pages/contact'
